f1d464545d0e96c1decf6e7c2cb2785d823c7e7e
Mavula06 <mahlalela06@yahoo.com>
2026-08-07 04:12:56 -0700

    Add files via upload

--- loading.tsx
+++ loading.tsx
@@ -1,6 +1,6 @@
 import { Loader2 } from 'lucide-react';
 
-export default function Loading() {
+export default function DashboardLoading() {
   return (
     <div className="flex min-h-[60vh] items-center justify-center">
       <Loader2 className="h-8 w-8 animate-spin text-primary" />
